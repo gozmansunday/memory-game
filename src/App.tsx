f160c6f4c530592832851dabee77b4f575a59d4f
Gozman <thegzmn@gmail.com>
2023-04-27 02:21:04 +0100

    Init React + TS + Tailwind project via Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="font-clash text-center text-3xl">
+        Memory Game
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+
+  theme: {
+    screens: {
+      xxs: '324px',
+      xs: '356px',
+      sm: '400px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+      '2xl': '1800px',
+    },
+    extend: {
+      colors: {
+        colorName: '#color',
+      },
+      fontFamily: {
+        clash: ['Clash Display', 'sans-serif'],
+      },
+      boxShadow: {
+        shadow: '0px 0px 0px 0px rgba(0, 0, 0, 0.0)',
+      },
+    },
+  },
+
+  plugins: [],
+}
