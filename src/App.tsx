9d4c84bfb3d80f57d6d620de999003c550812a17
Gozman <thegzmn@gmail.com>
2023-04-27 23:30:55 +0100

    Update fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <div className="font-clash text-center text-3xl">
+      <div className="font-bebas text-center text-3xl">
         Memory Game
       </div>
     </>
