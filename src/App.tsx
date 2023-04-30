b9344ebce3385e917ac9c1aba1780361e9fbe164
Gozman <thegzmn@gmail.com>
2023-04-30 02:23:26 +0100

    Use bg images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 const App = () => {
-  return (
-    <>
-      <div className="font-bebas text-center text-3xl">
-        Memory Game
+  const bgInteger = Math.floor(Math.random() * 3) + 1;
+
+  return (  
+    <div className="relative">
+      {/* Wallpaper and Overlay */}
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat h-screen"
+        style={{ backgroundImage: `url('../src/assets/images/backgrounds/bg-img-${bgInteger}.jpeg')` }}
+      ></div>
+      <div className="absolute inset-0 bg-black/50 h-screen"></div>
+
+      {/* Page Content */}
+      <div className="relative font-bebas text-center">
+        <h1 className="py-3 text-neutral-200 text-5xl">
+          Memory Game
+        </h1>
       </div>
-    </>
+    </div>
   )
 }
 
