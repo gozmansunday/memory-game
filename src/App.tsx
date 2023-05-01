9aed4a556ff3a392281db28371ab38606ab4b3bc
Gozman <thegzmn@gmail.com>
2023-05-01 03:20:18 +0100

    Update ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
+import Header from "./components/Header";
+import CardSection from "./components/CardSection";
+
 const App = () => {
-  return (  
+  return (
     <div className="relative">
       {/* Wallpaper and Overlay */}
       <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat h-screen"
+        className="absolute inset-0 bg-cover bg-fixed bg-center bg-no-repeat"
         style={{ backgroundImage: `url('/images/backgrounds/bg-img.jpeg')` }}
       ></div>
-      <div className="absolute inset-0 bg-black/25 h-screen"></div>
+      <div className="absolute inset-0 bg-black/50"></div>
 
       {/* Page Content */}
-      <div className="relative font-bebas text-center">
-        <h1 className="py-3 text-neutral-200 text-5xl">
-          Memory Game
-        </h1>
+      <div className="relative text-neutral-300">
+        <Header />
+        <p className="mt-3 font-clash text-center text-sm sm:text-base md:text-xl font-semibold text-neutral-400">
+          Choose cards to get points, but do not choose the same card twice.
+        </p>
+        <CardSection />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
