a7d07833ea602545cbda35ee60c313f7ec35f039
Gozman <thegzmn@gmail.com>
2023-05-02 23:52:26 +0100

    Implement shuffle cards on card click

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+
 import Card from "./Card";
 import imageUrls from "./images";
 
 const CardSection = () => {
+  const [imageUrlArray, setImageUrlArray] = useState<string[]>(imageUrls);
+
+  const handleShuffleArray = () => {
+    const array = [...imageUrlArray]
+
+    for (let i = 0; i < array.length; i++) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    setImageUrlArray(array);
+  };
+
   return (
     <section className="flex flex-wrap justify-center gap-x-8 gap-y-12 max-w-[90rem] py-8 px-4 mx-auto sm:py-10 md:py-12">
-      {imageUrls.map((imageUrl) => (
-        <Card url={imageUrl} /> 
+      {imageUrlArray.map((imageUrl) => (
+        <div onClick={handleShuffleArray}>
+          <Card
+            key={imageUrl}
+            url={imageUrl}
+          /> 
+        </div>
       ))}
     </section>
   );
