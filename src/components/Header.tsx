e1bad77c8c1f70fcedd4cda8ccc856a15e208d17
Gozman <thegzmn@gmail.com>
2023-05-04 00:25:14 +0100

    Update UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const Header = ({score, highscore}: Props) => {
   return (
-    <header className="background-image bg-cover font-bebas sticky top-0 z-50 py-3 border-b border-neutral-400 md:px-1 lg:px-0">
+    <header className="header-background-image bg-cover font-bebas sticky top-0 z-50 py-3 border-b border-neutral-300 md:px-1 lg:px-0">
       <div className="container mx-auto md:flex md:items-center md:justify-between">
         <h1 className="text-3xl text-center sm:text-4xl md:text-5xl">
             Memory Game
